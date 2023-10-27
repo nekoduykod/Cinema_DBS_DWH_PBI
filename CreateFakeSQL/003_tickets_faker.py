35bbfe32fe7cf599f8c5bc6d0c4080b22448fa3a
Oleksandr Zaliubovskyi <135022571+nekoduykod@users.noreply.github.com>
2023-10-27 17:45:28 +0300

    Update 003_tickets_faker.py

--- CreateFakeSQL/003_tickets_faker.py
+++ CreateFakeSQL/003_tickets_faker.py
@@ -1,25 +1,18 @@
 import faker
 import os
 
-# Directory to load
 os.chdir('D:\PROJECTS\Cinema_DBS_DWH_PBI\InsertFakeSQL')
 
-# Create a Faker object
 fake = faker.Faker()
-
-# Initialize the 'id' variable
 id = 1
 
-# Generate a list of fake tickets
 tickets = []
 cin_id_values = list(range(1, 10))  
 cinhall_id_values = list(range(1, 8))  
 
-# Initialize counters for cin_id and cinhall_id
 cin_id_counter = 1
 cinhall_id_counter = 1
 
-# Continue generating entries until we reach 1000 inserts
 while id <= 1000:
     cin_id = cin_id_counter
     cinhall_id = cinhall_id_counter
@@ -36,11 +29,8 @@ while id <= 1000:
         'purch_date': fake.date_time_this_decade()
     }
     tickets.append(ticket)
-
-    # Increment 'id' for the next record
+    
     id += 1
-
-    # Update cin_id and cinhall_id counters, and start over if needed
     cinhall_id_counter += 1
     if cinhall_id_counter > 7:
         cinhall_id_counter = 1
@@ -48,7 +38,6 @@ while id <= 1000:
         if cin_id_counter > 9:
             cin_id_counter = 1
 
-# Generate insert queries for each ticket
 insert_queries = []
 for ticket in tickets:
     insert_query = """
@@ -60,7 +49,6 @@ for ticket in tickets:
     )
     insert_queries.append(insert_query)
 
-# Write the insert queries to a file
 with open('003_ticket_queries.sql', 'w') as f:
     for insert_query in insert_queries:
         f.write(insert_query + '\n')
