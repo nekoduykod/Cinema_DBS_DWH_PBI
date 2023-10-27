814769c79f347c3edf21bddace69a79a43f78c6e
Oleksandr Zaliubovskyi <135022571+nekoduykod@users.noreply.github.com>
2023-10-27 17:43:40 +0300

    Update 000_create_databases.sql

--- CreateDBsInPostgre/000_create_databases.sql
+++ CreateDBsInPostgre/000_create_databases.sql
@@ -1,13 +1,11 @@
--- Для films таблиці 
 CREATE DATABASE movies;
-
--- Для tickets, timetable, users, cinemas таблиці    
+ 
 CREATE DATABASE schedules;
 
--- Для кінцевої об'єднаної таблиці aka cinena_data "data warehouse"
 CREATE DATABASE redshift_dev;
  
 
 -- Налаштуйте user owner. Обрав 'postgres' user з паролем, який треба для .py-script`s connection   
 -- Коли таблиці створите, наповніть даними з InsertFakeSQL папки
+-- до речі, cinemas table можна в іншій базі даних створити, якщо треба. Відповідно, .py-скрипт відкорегувати
 -- Далі, .py скрипт - до ваших послуг '''
